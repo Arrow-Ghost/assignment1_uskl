2076be21e3cac114ebe5abbd605d3f7366aff499
Shrish Verma <shrishverma0001@gmail.com>
2025-10-21 14:03:13 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,10 @@
+x=int(input("Enter 1st number:"))
+y=int(input("Enter 2nd number:"))
+addition=x+y
+subtraction=x-y
+multiplication=x*y
+division=x/y
+print("Addition:",addition)
+print("Subtraction:",subtraction)
+print("Multiplication:",multiplication)
+print("Division:",division)
